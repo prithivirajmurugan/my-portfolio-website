e1f3a7dc677fe27c9de354666875a55c27e058e1
Prithiviraj Murugan (RBEI/ECD42-EC) <Murugan.Prithiviraj@in.bosch.com>
2021-10-05 21:46:17 +0530

    created forms - fixes

--- src/components/body/contact/contact-form/ContactForm.js
+++ src/components/body/contact/contact-form/ContactForm.js
@@ -3,7 +3,7 @@ import './ContactForm.css';
 
 function ContactForm() {
     return (
-        <form method ="POST" data-netlify="true">
+        <form name="contact" method ="POST" data-netlify-recaptcha="true" data-netlify="true">
              <input type="text" name="name" placeholder="Your name" className="contact-form-input" />
             <input type="email" name="email" placeholder="Your email address" className="contact-form-input" />
             <textarea rows="5" cols="30" name="description" placeholder="Your message" className="contact-form-input"></textarea>
