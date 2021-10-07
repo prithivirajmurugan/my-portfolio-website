a0e9917636f43f3b3de1860079fa8660780fb835
Prithiviraj Murugan (RBEI/ECD42-EC) <Murugan.Prithiviraj@in.bosch.com>
2021-10-07 10:03:41 +0530

    Contact form changes

--- src/components/body/contact/contact-form/ContactForm.js
+++ src/components/body/contact/contact-form/ContactForm.js
@@ -3,8 +3,7 @@ import './ContactForm.css';
 
 function ContactForm() {
     return (
-        <form name="contact-form" method="post" data-netlify="true" onSubmit="submit">
-            <input type="hidden" name="form-name" value="contact-form"/>
+        <form name="contact-form" method="post" netlify onSubmit="submit">
              <input type="text" name="name" placeholder="Your name" className="contact-form-input" required />
             <input type="email" name="email" placeholder="Your email address" className="contact-form-input" required />
             <textarea rows="5" cols="30" name="description" placeholder="Your message" className="contact-form-input" required></textarea>
